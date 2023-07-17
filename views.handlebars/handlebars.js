dc3e5994896fc387117a1f0cf7ceb14f1fa801a8
Jfrancis0518 <jfrancis0518@gmail.com>
2023-07-17 11:53:05 -0400

    handlebars

---
+++ views.handlebars/handlebars.js
@@ -0,0 +1,14 @@
+$(documnt).ready(function()) {
+
+    var source = $("#genre-template").html();
+    var template = Handlebars.complie(source);
+
+    var source = $().html();
+    var template = Handlebars.complie(source);
+
+    var context = {
+        genre: {
+            type: "Action" 
+        }
+    }
+}
