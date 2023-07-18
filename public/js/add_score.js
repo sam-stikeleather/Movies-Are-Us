99560fbd9ab3d0246aa896d1b0d3776e92930bb1
Sam <sam.stikeleather@gmail.com>
2023-07-18 13:16:43 -0400

    modified add_score.js

---
+++ public/js/add_score.js
@@ -0,0 +1,24 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+    const movie_title = document.querySelector('#movie_title').value;
+    const score = document.querySelector('#score').value;
+
+    const response = await fetch(`/movie`, {
+        method: 'POST',
+        body: JSON.stringify({
+            movie_title,
+            score,
+        }),
+        headers: {
+            'Content_Type' : 'application/json',
+        },
+    });
+    //if the score is added, the 'all' template will be rendered
+    if  (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to add score');
+    }
+}
+
+document.querySelector('.new_movie_score').addEventListener('submit', newFormHandler);
