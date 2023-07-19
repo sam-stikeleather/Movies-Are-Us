864a531494b8c74fd25513fbc833d600bb32a7a4
Sam <sam.stikeleather@gmail.com>
2023-07-19 18:14:04 -0400

    added profile handlebars and movie.js

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,1 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var movies = [];
 
+  // Function to render the table with the provided data
+  function renderTable(data) {
+    movies = data.movies;
+
+    var movieTableTemplate = Handlebars.compile(
+      document.getElementById('movie-table-template').innerHTML
+    );
+    var html = movieTableTemplate({ movies: movies });
+    document.getElementById('movie-table-body').innerHTML = html;
+
+    // Add click event listeners to sortable buttons
+    var sortableButtons = document.querySelectorAll('.sortable');
+    sortableButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var sortKey = button.dataset.sortKey;
+        sortMovies(sortKey);
+      });
+    });
+  }
+
+  // Function to sort the movies based on the selected key
+  function sortMovies(key) {
+    movies.sort(function(a, b) {
+      var valueA = a[key];
+      var valueB = b[key];
+
+      if (key === 'title') {
+        valueA = valueA.toLowerCase();
+        valueB = valueB.toLowerCase();
+      }
+
+      if (valueA < valueB) return -1;
+      if (valueA > valueB) return 1;
+      return 0;
+    });
+
+    renderTable({ movies: movies });
+  }
+
+  // Fetch movie data from movieData.json
+  fetch('movieData.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      renderTable(data);
+    })
+    .catch(function(error) {
+      console.log('Error fetching movie data:', error);
+    });
+});
