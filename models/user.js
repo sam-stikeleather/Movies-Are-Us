a9280bb52e2b4950590049ab7390d84441a3ba50
Austin Shook <austinleeshook@gmail.com>
2023-07-19 14:04:27 -0400

    Editted login/signup code and paths

--- models/user.js
+++ models/user.js
@@ -46,9 +46,13 @@ User.init(
   },
   {
     hooks: {
-      async beforeCreate(newUserData) {
+      beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
     },
     sequelize,
